c59a16ba7f71fa1a069c1fe197c6fc1b4adaf909
Pedro Huertas <pmh242@gmail.com>
2019-10-21 23:36:33 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -23,7 +23,7 @@ end
 
 def remove_element_from_start_of_array(array)
   sixth_array = ["wow", "I", "am", "really", "learning", "arrays!"]
-  item_removed_variable = sixth_array.shift
+  variable_for_removed_item = sixth_array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
