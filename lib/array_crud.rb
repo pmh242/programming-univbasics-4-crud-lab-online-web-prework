f127a801c6d68f778f1cbec786172443cd512fe7
Pedro Huertas <pmh242@gmail.com>
2019-10-21 23:30:17 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -18,7 +18,7 @@ end
 
 def remove_element_from_end_of_array(array)
   fifth_array = ["I", "am", "really", "learning", "arrays!"]
-  fifth_array.pop("")
+  fifth_array.pop("arrays!")
 end
 
 def remove_element_from_start_of_array(array)
