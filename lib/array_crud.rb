9d908b147a4965885f892fbdf07e685ad12b1077
Pedro Huertas <pmh242@gmail.com>
2019-10-21 23:37:57 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -28,7 +28,7 @@ end
 
 def retrieve_element_from_index(array, index_number)
   seventh_array = ["wow", "I", "am", "really", "learning", "arrays!"]
-  seventh_array.index(2)
+  seventh_array[2]
 end
 
 def retrieve_first_element_from_array(array)
